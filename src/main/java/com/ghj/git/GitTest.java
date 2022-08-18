9c53f28c2b87f496761a1aa02986a27510a3c51f
xiaojian <xiaojian@git.com>
2022-08-18 17:11:03 +0800

    third commit

--- src/main/java/com/ghj/git/GitTest.java
+++ src/main/java/com/ghj/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git 第二次修改");
+        System.out.println("hello git 第三次修改");
+
     }
 }
