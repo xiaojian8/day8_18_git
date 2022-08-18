163d13bca29944b3fce4c167457363422d2db2c9
xiaojian <xiaojian@git.com>
2022-08-18 17:28:42 +0800

    master five commit

--- src/main/java/com/ghj/git/GitTest.java
+++ src/main/java/com/ghj/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git 第二次修改");
         System.out.println("hello git 第三次修改");
         System.out.println("hello git 第四次修改，这次是在hot-fix下修改的");
+        System.out.println("hello git 第五次修改，这次是在master下验证冲突");
 
     }
 }
