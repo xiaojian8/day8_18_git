2b78f989b744c834175f269157d1b8336928341d
xiaojian <xiaojian@git.com>
2022-08-18 17:09:56 +0800

    second commit

--- src/main/java/com/ghj/git/GitTest.java
+++ src/main/java/com/ghj/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ghj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git 第二次修改");
     }
 }
